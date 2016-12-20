5c022d1d90d7c9a7e666529cff9ce740fa039f9a
Nathan Collins <nathanc@crossfields.com.au>
2016-12-21 07:51:56 +1000

    Added audio properties and playing options

--- crossfields-audio.js
+++ crossfields-audio.js
@@ -1,12 +1,68 @@
 Polymer({
-
   is: 'crossfields-audio',
 
   properties: {
-    prop1: {
-      type: String,
-      value: 'crossfields-audio',
+    file: {
+      type: Array,
+      notify: true,
+      observer: 'playSound'
     },
+
+    autoplay: {
+      type: Boolean,
+      value: false
+    },
+
+    loop: {
+      type: Boolean,
+      value: false
+    },
+
+    volume: {
+      type: Number,
+      value: 0.5
+    },
+
+    waitForLoad: {
+      type: Boolean,
+      value: false
+    },
+
+    fadeOut: {
+      type: Boolean,
+      value: false
+    }
   },
 
+  playSound: function(file) {
+    var self = this;
+    var sound = new Howl(
+      {
+        src: file,
+        autoplay: self.autoplay,
+        loop: self.loop,
+        volume: self.volume
+      }
+    );
+
+    sound.on('end', function() {
+      self.fire('audio-ended');
+    });
+
+    sound.on('play', function() {
+      self.fire('audio-started');
+    });
+
+    if(!this.waitForLoad) {
+      sound.play();
+    }
+
+    sound.once('load', function() {
+        sound.play();
+    });
+
+    if(this.fadeOut) {
+      sound.fade(1, 0, 1000);
+    }
+  }
 });
